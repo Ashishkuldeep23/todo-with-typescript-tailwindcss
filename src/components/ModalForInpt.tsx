15f06d3a8ed3e963a8f6b45ef6644fc0a322e933
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-10-09 10:35:23 +0530

    OnkeyDown event added

--- src/components/ModalForInpt.tsx
+++ src/components/ModalForInpt.tsx
@@ -1,4 +1,4 @@
-import React , {useContext, useEffect , useRef} from 'react'
+import React, { useContext, useEffect, useRef, KeyboardEvent } from 'react'
 import { TInputOfTodo } from './MainTodo';
 import ThemeContext from './ThemeContext';
 
@@ -19,17 +19,16 @@ type TModalProps = {
 
 const ModalForInpt = ({ setModalVisiable, modalVisiable, addNewTodo, updatingTodo, input, setInput, actualToDoUpadateFunction }: TModalProps) => {
 
-  const theme =  useContext<boolean>(ThemeContext);
+  const theme = useContext<boolean>(ThemeContext);
 
-  const inputRef = useRef<HTMLInputElement>(null)     
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
   // // // So if you using useRef with input then we need to decide a generic for that , that is HTMLInputElement type like above , otherwise it'll give you error in focus()
 
 
   // const [modalVisiable , setModalVisiable] = useState<boolean>(false)
-
   // const [input, setInput] = useState<TInputOfTodo>({ heading: "", content: "" })
-
-
   // // // New method used to focus() input tag , see the first input box ---> ( arrow fn used in ref prop of input.)
 
 
@@ -43,16 +42,15 @@ const ModalForInpt = ({ setModalVisiable, modalVisiable, addNewTodo, updatingTod
     e.stopPropagation()
 
 
-      // console.log(``)
+    // console.log(``)
 
-      // // // Two thing todo 1) focus on textArea if press enter in input , 2) Call clickHandler if Enter press on textArea.
+    // // // Two thing todo 1) focus on textArea if press enter in input , 2) Call clickHandler if Enter press on textArea.  // // Now using Onkeydown Fn for this.
 
-      // // 2nd => Call clickHandler if Enter press on textArea.
-      if(e.target.name === "content"  && e.target.value === `${input.content}\n`){
-
-        // // // `${input.content}\n` by this way we  can read enter btn in textArea of TS
-        handleClick(e)  // // // Calling clickhandler fn and also changes type of e in click handler.
-      }
+    // // 2nd => Call clickHandler if Enter press on textArea.
+    // if (e.target.name === "content" && e.target.value === `${input.content}\n`) {
+    //   // // // `${input.content}\n` by this way we  can read enter btn in textArea of TS
+    //   handleClick(e)  // // // Calling clickhandler fn and also changes type of e in click handler.
+    // }
 
 
 
@@ -60,8 +58,9 @@ const ModalForInpt = ({ setModalVisiable, modalVisiable, addNewTodo, updatingTod
   }
 
 
-  // MouseEvent<HTMLButtonElement, MouseEvent>
-  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent> | Event) {
+  // MouseEvent<HTMLButtonElement, MouseEvent>  
+  // // // Evnet type of clickHandler will depend on many types ---> check below.
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent> | KeyboardEvent<HTMLTextAreaElement | HTMLImageElement> | Event) {
     e.preventDefault()
     e.stopPropagation()
 
@@ -82,32 +81,32 @@ const ModalForInpt = ({ setModalVisiable, modalVisiable, addNewTodo, updatingTod
       addNewTodo(input); setModalVisiable(false);
     }
 
-
   }
 
 
-  useEffect( ()=>{
+
+  useEffect(() => {
 
     // // // first time focus on input --->
 
-    if(modalVisiable){
+    if (modalVisiable) {
       inputRef?.current?.focus()
     }
-    else{
+    else {
       // // // This else part is very use full , it cleans input and make it empty (BUG fixed)
       setInput({ id: "", heading: "", content: "", isFav: false })
     }
 
-  } , [modalVisiable] )
+  }, [modalVisiable])
 
 
 
   return (
-    <div className={`h-full w-screen border  ${!theme ? "bg-blue-300" : "bg-sky-900"} flex flex-col justify-center items-center absolute transition-all  
-       ${modalVisiable ? " top-0 z-50" : ' top-full -z-50 hidden'} `}
+    <div className={`h-full w-screen border  ${!theme ? "bg-blue-300" : "bg-sky-900"} flex flex-col  items-center absolute transition-all  
+       ${modalVisiable ? " top-0 z-50" : ' top-full -z-50 hidden'}  justify-start sm:justify-center  `}
     >
 
-      <div className=' animate__animated  animate__flipInX  w-11/12 md:w-1/4 p-2 bg-orange-300 border border-red-500 rounded-2xl flex flex-col text-center py-8 relative'>
+      <div className=' mt-24 sm:mt-0 animate__animated  animate__flipInX  w-11/12 md:w-1/4 p-2 bg-orange-300 border border-red-500 rounded-2xl flex flex-col text-center py-8 relative  '>
         <button
           className=' animate__animated animate__zoomInLeft animate__slow absolute right-3 top-2 border px-2 text-white font-bold rounded-lg bg-red-500 hover:bg-red-700 hover:scale-110 active:scale-90 transition-all '
           onClick={() => { setModalVisiable(false) }}
@@ -120,24 +119,31 @@ const ModalForInpt = ({ setModalVisiable, modalVisiable, addNewTodo, updatingTod
         >TODO Heading ⬇️</label>
 
         <input
-         ref={inputRef}
+          ref={inputRef}
+          onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
+            if (e.key === "Enter") {
+              // alert("f")
+              textAreaRef.current?.focus()
+            }
+          }}
           className='rounded focus:bg-green-200'
           type="text" id='heading' name="heading"
           onChange={onChangeHandler}
           value={input.heading}
           placeholder='TODO Heading'
-          // ref={todoHeading}
-          //   ref = {(inp) => {
-          //     inp?.focus();
-          //  }}
-
+        // ref={todoHeading}
+        //   ref = {(inp) => {
+        //     inp?.focus();
+        //  }}
         />
 
         <label
           htmlFor="content"
           className='text-left  mt-4'
         >TODO Heading ⬇️</label>
+
         <textarea
+          ref={textAreaRef}
           className='rounded focus:bg-green-200 max-h-80 min-h-5 '
           name="content"
           onChange={onChangeHandler}
@@ -145,6 +151,11 @@ const ModalForInpt = ({ setModalVisiable, modalVisiable, addNewTodo, updatingTod
           placeholder='TODO Content'
           value={input.content}
           rows={5}
+          onKeyDown={(e: KeyboardEvent<HTMLTextAreaElement>) => {
+            if (e.key === "Enter") {
+              handleClick(e)
+            }
+          }}
         ></textarea>
 
         <button
